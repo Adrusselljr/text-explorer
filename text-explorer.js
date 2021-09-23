cfc736dcf370e552b0576368c9cfd38fef77ab9e
Aaron Russell Jr <adrusselljr@Aarons-MBP.lan>
2021-09-23 15:15:19 -0400

    edit js

--- text-explorer.js
+++ text-explorer.js
@@ -1,26 +1,79 @@
 const prompt = require("prompt-sync")({ sigint: true })
 
-let x = 0
-let y = 0
+let x = -2
+let y = -2
 
-while(true) {
+function reset() {
+    x = -2
+    y = -2
+}
 
+while(x !== 2 && y !== 2) {
 
     const direction = prompt("Which direction would you like to go (Up, Down, Right, Left)? ")
 
     if(direction === "up") {
-        y = y + 1
+        y += 1
     }
-    if(direction === "down") {
-        y = y - 1
+    else if(direction === "down") {
+        y -= 1
     }
-    if(direction === "right") {
-        x = x - 1
+    else if(direction === "right") {
+        x += 1
     }
-    if(direction === "left") {
-        x = x + 1
+    else if(direction === "left") {
+        x -= 1
+    }
+    else {
+        reset()
+        console.log("INVALID CHOICE!, You Just reset your position!")
+    }
+
+    if(x === -2 && y === -2) {
+        console.log("Start Here!")
+    }
+    else if(x === -2 && y === -1) {
+        console.log("You walk in the front door")
+    }
+    else if(x === -1 && y === -2) {
+        console.log("You sit on the front porch")
+    }
+    else if(x === -1 && y === -1) {
+        console.log("You sit on the couch")
+    }
+    else if(x === -1 && y === 0) {
+        console.log("You use the bathroom")
+    }
+    else if(x === 0 && y === -1) {
+        console.log("You take a shower")
+    }
+    else if(x === 0 && y === 0) {
+        console.log("You go to the kitchen")
+    }
+    else if(x === 1 && y === 0) {
+        console.log("You grab some chips")
+    }
+    else if(x === 0 && y === 1) {
+        console.log("You stub your toe on the dinning room table")
+    }
+    else if(x === 1 && y === 1) {
+        console.log("You start playing Call Of Duty")
+    }
+    else if(x === 1 && y === 2) {
+        console.log("You turn on Netflix")
+    }
+    else if(x === 2 && y === 1) {
+        console.log("You are tired")
+    }
+    else if(x === 2 && y === 2) {
+        console.log("You go to sleep")
+    }
+    else {
+        console.log("ARE YOU LOST!?")
     }
 
     console.log([x, y])
 
 }
+
+console.log("Thanks For Playing!")
